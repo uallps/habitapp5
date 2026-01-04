d44242058d144bf4d79459614817784607328dc1
Oliver Diaz Veiga <odv717@inlumine.ual.es>
2026-01-04 20:26:52 +0100

    protocolo insatisfecho fix 1

--- TaskApp/HabitApp/Features/Game/GamePlugin.swift
+++ TaskApp/HabitApp/Features/Game/GamePlugin.swift
@@ -8,10 +8,19 @@
 import SwiftUI
 
 /// Plugin de gamificación que añade mecánicas de juego a los hábitos
-final class GamePlugin: FeaturePlugin {
+final class GamePlugin: ObservableObject, FeaturePlugin {
     static let shared = GamePlugin()
     
-    private init() {}
+    @Published var isEnabled: Bool {
+        didSet {
+            UserDefaults.standard.set(isEnabled, forKey: "feature_game_enabled")
+            NotificationCenter.default.post(name: .featureToggled, object: nil)
+        }
+    }
+    
+    private init() {
+        self.isEnabled = UserDefaults.standard.bool(forKey: "feature_game_enabled")
+    }
     
     // MARK: - FeaturePlugin Protocol
     
@@ -31,28 +40,16 @@ final class GamePlugin: FeaturePlugin {
         "gamecontroller.fill"
     }
     
-    var isEnabled: Bool {
-        get {
-            UserDefaults.standard.bool(forKey: "feature_game_enabled")
-        }
-        set {
-            UserDefaults.standard.set(newValue, forKey: "feature_game_enabled")
-            NotificationCenter.default.post(name: .featureToggled, object: nil)
-        }
-    }
-    
     var menuOrder: Int {
         2 // Después de Rutinas (1) pero antes de otros plugins futuros
     }
     
-    @ViewBuilder
-    func makeView() -> some View {
-        GameView()
+    func makeView() -> AnyView {
+        AnyView(GameView())
     }
     
-    @ViewBuilder
-    func makeSettingsView() -> some View {
-        GameSettingsView()
+    func makeSettingsView() -> AnyView {
+        AnyView(GameSettingsView())
     }
 }
 
