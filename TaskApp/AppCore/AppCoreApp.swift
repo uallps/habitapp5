edec6baae9fb9c147593697c88a1e376bfb6619d
Miranda Ibáñez José <jmi240@inlumine.ual.es>
2026-01-10 01:57:59 +0100

    Adición de los targets, uno para la app solo con core (Incluye las feature obligatorias del arbol del aula) y otro para la app completa (features obligatorias + plug ins)

---
+++ TaskApp/AppCore/AppCoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppCoreApp.swift
+//  AppCore
+//
+//  Created by Aula03 on 10/1/26.
+//
+
+import SwiftUI
+
+
+struct AppCoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TaskApp/AppFull/AppFullApp.swift
@@ -0,0 +1,16 @@
+//
+//  AppFullApp.swift
+//  AppFull
+//
+//  Created by Aula03 on 10/1/26.
+//
+
+import SwiftUI
+
+struct AppFullApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
